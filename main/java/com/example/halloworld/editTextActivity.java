7a11a72b262d1edf9831a795be7319eb88f39f35
Sp1Kez <57507245+Sp1Kez@users.noreply.github.com>
2019-12-11 18:14:49 +0000

    Add files via upload
    user login 
    scrollView

---
+++ main/java/com/example/halloworld/editTextActivity.java
@@ -0,0 +1,14 @@
+package com.example.halloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text);
+    }
+}
